2b582d5d0738d6cc863bc5e8e88ca4bb1074fcfe
Ryan Coughlin <ryan@ryancoughlin.com>
2026-03-21 22:23:59 -0400

    fix: add missing Modifier import in MainActivity

--- app/src/main/java/com/example/saltyoffshore/MainActivity.kt
+++ app/src/main/java/com/example/saltyoffshore/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
